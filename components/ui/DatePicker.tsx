1e53517dda84baa516552a86f7c3117a83b83918
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2025-04-29 20:23:38 +0400

    booking modal fixes

--- components/ui/DatePicker.tsx
+++ components/ui/DatePicker.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import Datepicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import { format } from 'date-fns'
 
 interface Props {
   date: Date | null
@@ -17,8 +16,8 @@ export const DatePicker = ({ date, onChange }: Props) => {
         selected={date}
         onChange={onChange}
         dateFormat="PPP"
-        className="w-full border rounded px-3 py-2 text-sm focus:outline-none focus:ring"
         placeholderText="Pick a date"
+        className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
   )
--- components/ui/TimePicker.tsx
+++ components/ui/TimePicker.tsx
@@ -20,8 +20,8 @@ export const TimePicker = ({ time, onChange }: Props) => {
         timeIntervals={15}
         timeCaption="Time"
         dateFormat="h:mm aa"
-        className="w-full border rounded px-3 py-2 text-sm focus:outline-none focus:ring"
         placeholderText="Pick a time"
+        className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
   )
